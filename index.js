874b72b72a58c23ef766999f3788ff71b946effb
dtmoyaji <156999048+tax-mux@users.noreply.github.com>
2024-03-06 09:08:13 +0900

    modified:   .env.sample 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -3,12 +3,27 @@
  */
 
 // Import the lineworks_simple_message module
+require('dotenv').config();
 const { sendMessage } = require("./lineworks_simple_message");
+const express = require("express");
+const app = express();
+let to;
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+    let emoji = (req.body.execution.status=="succeeded")? "🆗" : "🆖";
+    let message = `${emoji} Project: ${req.body.execution.project}\nstatus: ${req.body.execution.status}`;
+    message += `\n\n${req.body.execution.job.description}`;
+    await sendMessage(to, message);
+    console.log(req.body);
+    res.send("OK");
+});
 
 async function main() {
     let argv = process.argv;
-    let to;
     let message;
+    let port;
 
     for (let i = 0; i < argv.length; i++) {
         if (argv[i] === "-to") {
@@ -17,9 +32,26 @@ async function main() {
         if (argv[i] === "-message") {
             message = argv[i + 1];
         }
+        if (argv[i] === "-port") { // ポート番号を指定すると、webhookサーバーを起動する。
+            port = argv[i + 1];
+        }
     }
-    // Send a message
-    await sendMessage(to, message);
+
+    // パラメータをチェックして、問題が無ければメッセージを送信する。
+    if (port !== undefined && to !== undefined) {
+        // Start the webhook server
+        app.listen(port, () => {
+            console.log(`Example app listening at ${process.env.WEBHOOK_URL}:${port}`);
+        });
+    } else if (to !== undefined && message !== undefined) {
+        await sendMessage(to, message);
+    } else {
+        console.log("Invalid parameters.");
+        console.log("Usage1: direct message");
+        console.log("    node index.js -to [to] -message [message]");
+        console.log("Usage2: start webhook server");
+        console.log("    node index.js -port [port] -to [to]");
+    }
 }
 
 main();
